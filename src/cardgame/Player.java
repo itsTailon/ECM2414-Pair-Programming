c47d14e37a1843456e83c2960ab20555a121a11c
itsTailon <philip.tsitlakidis@outlook.com>
2025-10-10 12:50:53 +0100

    Adding functionality to methods related to saving player log messages to local text files.

--- src/cardgame/Player.java
+++ src/cardgame/Player.java
@@ -1,6 +1,10 @@
 package cardgame;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+
 
 public class Player implements Runnable {
     private Deck drawDeck;
@@ -22,9 +26,38 @@ public class Player implements Runnable {
         //TODO: Implement and ensure it is thread-safe.
     }
 
+    /**
+     * Saving log contents to log file.
+     * @param filename name of file to store log information on.
+     */
     public void saveLogFile(String filename) {
-        //TODO: Implement.
+        try {
+            // Create new file log if it doesn't exist.
+            File logFile = new File(filename);
+            logFile.createNewFile();
+
+            // Iterate through log lines and add to output String to be written to log.
+            String logOutput = "";
+            for (String line : logLines) {
+                logOutput += line + "\n";
+            }
+
+            // Writing to the log file.
+            FileWriter fileWriter = new FileWriter(logFile);
+            fileWriter.write(logOutput);
+            fileWriter.close();
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
+    /**
+     * Adding a log message to logLines array.
+     * @param logMessage Message to be passed to logLines array.
+     */
+    public void log(String logMessage) {
+        this.logLines.add(logMessage);
+    }
 
 }
