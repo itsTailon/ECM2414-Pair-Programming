bc52bb4f6df931d357cbd11d37856cc57b5af0ca
Angelos Tsangarides <angelos@tsangarides.net>
2025-11-01 16:00:45 +0000

    Implemented unit test for Player.drawAndDiscard.
    Updated dev log.

--- src/tests/PlayerTest.java
+++ src/tests/PlayerTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import cardgame.Card;
 import cardgame.Deck;
 import cardgame.Player;
 import org.junit.jupiter.api.Test;
@@ -8,9 +9,10 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PlayerTest {
 
@@ -41,5 +43,50 @@ public class PlayerTest {
         assertEquals("line 2", lines[1]);
         assertEquals("line 3", lines[2]);
 
+    }
+
+    @Test
+    public void testDrawAndDiscard() throws NoSuchFieldException, IllegalAccessException {
+        // Setup decks
+        Deck drawDeck = new Deck();
+        Deck discardDeck = new Deck();
+        Card sampleCardInDeck = new Card(3);
+        drawDeck.insert(sampleCardInDeck);
+
+        Player player = new Player(drawDeck, discardDeck, 1);
+        Card preferredCard = new Card(1);
+        player.insertIntoHand(preferredCard);
+
+        int cardsInDrawDeckBeforeDrawAndDiscardInvoked = drawDeck.size();
+
+        player.drawAndDiscard();
+
+        // Use Reflection to access the private 'hand' field of the Player object
+        Field hand = Player.class.getDeclaredField("hand");
+        hand.setAccessible(true);
+        ArrayList<Card> playerHand = (ArrayList<Card>) (hand.get(player));
+
+        int cardsInHandBeforeDrawAndDiscardInvoked = playerHand.size();
+
+        // Check that player does not discard their preferred card
+        //
+        //      The player started with only their preferred card in their hand
+        //      and the card that they drew should be a '3', which they thus
+        //      should have discarded, leaving only the preferred card
+        //      (preferredCard) in their hand.
+        //
+        assertTrue(playerHand.contains(preferredCard));
+
+        // Ensure that the draw deck has lost a card
+        assertEquals(1, cardsInDrawDeckBeforeDrawAndDiscardInvoked - drawDeck.size());
+
+        // Ensure that the player's hand has the same number as cards as they started with
+        assertTrue(cardsInHandBeforeDrawAndDiscardInvoked == ((ArrayList<Card>) (hand.get(player))).size());
+
+        // Ensure that the (prev. empty) discard deck increased in size by 1
+        assertEquals(1, discardDeck.size());
+
+        // Ensure that the discard deck now contains the discarded card (sampleCardInDeck)
+        assertTrue(discardDeck.draw() == sampleCardInDeck);
     }
 }
